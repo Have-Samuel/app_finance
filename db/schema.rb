9222ce2bb62bb493016a13396ef1d275a37a4211
Have Samuel <samiehave9@gmail.com>
2026-03-15 14:23:27 +0300

    Modify

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_14_200637) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_15_081040) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -32,6 +32,17 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_14_200637) do
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "account_id", null: false
+    t.decimal "amount", precision: 15, scale: 2
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.string "description"
+    t.datetime "transaction_date"
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_transactions_on_account_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email_address", null: false
@@ -42,4 +53,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_14_200637) do
 
   add_foreign_key "accounts", "users"
   add_foreign_key "sessions", "users"
+  add_foreign_key "transactions", "accounts"
 end
