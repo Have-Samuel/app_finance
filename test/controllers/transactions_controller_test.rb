2c94021f6db53c203022cac2a0b113598ae5e862
Have Samuel <samiehave9@gmail.com>
2026-03-18 04:23:47 +0300

    add Turbo Stream tests to test/controllers/transactions_controller_test.rb to verify the new   Hotwire functionality

--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -26,18 +26,33 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
     assert_equal "Transaction recorded.", flash[:notice]
   end
 
-  test "should destroy transaction and update balance" do
-    transaction = @account.transactions.create!(amount: 100.0, transaction_date: Date.today)
-    @account.reload
-    initial_balance = @account.balance
-
-    assert_difference("Transaction.count", -1) do
-      delete account_transaction_url(@account, transaction)
+  test "should create transaction via turbo_stream" do
+    assert_difference("Transaction.count") do
+      post account_transactions_url(@account), params: {
+        transaction: {
+          amount: 50.0,
+          category: "Food",
+          transaction_date: Date.today,
+          description: "Lunch"
+        }
+      }, as: :turbo_stream
     end
 
-    @account.reload
-    assert_equal initial_balance - 100.0, @account.balance
-    assert_redirected_to account_url(@account)
-    assert_equal "Transaction deleted.", flash[:notice]
+    assert_response :success
+    assert_match /turbo-stream action="prepend" target="transactions"/, response.body
+    assert_match /turbo-stream action="remove" target="no-transactions"/, response.body
+    assert_match /turbo-stream action="replace" target="account_#{@account.id}_balance"/, response.body
+  end
+
+  test "should destroy transaction via turbo_stream" do
+    transaction = @account.transactions.create!(amount: 100.0, transaction_date: Date.today)
+
+    assert_difference("Transaction.count", -1) do
+      delete account_transaction_url(@account, transaction), as: :turbo_stream
+    end
+
+    assert_response :success
+    assert_match /turbo-stream action="remove" target="transaction_#{transaction.id}"/, response.body
+    assert_match /turbo-stream action="replace" target="account_#{@account.id}_balance"/, response.body
   end
 end
